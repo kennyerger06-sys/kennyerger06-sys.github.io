a4d2c3bcf76f841e30935f7746ffaffd7a183b8b
kennyerger06-sys <kennyerger06@gmail.com>
2026-01-26 15:03:40 +0300

    Update snake.js

--- games/snake.js
+++ games/snake.js
@@ -1,1 +1,55 @@
+const canvas = document.getElementById("snakeGame");
+const ctx = canvas.getContext("2d");
 
+let box = 20;
+let snake = [{ x: 10 * box, y: 10 * box }];
+let food = { x: Math.floor(Math.random() * 19 + 1) * box, y: Math.floor(Math.random() * 19 + 1) * box };
+let d;
+
+document.addEventListener("keydown", direction);
+
+function direction(event) {
+    if(event.keyCode == 37 && d != "RIGHT") d = "LEFT";
+    else if(event.keyCode == 38 && d != "DOWN") d = "UP";
+    else if(event.keyCode == 39 && d != "LEFT") d = "RIGHT";
+    else if(event.keyCode == 40 && d != "UP") d = "DOWN";
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    for(let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = (i == 0) ? "#4ade80" : "white";
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+
+    ctx.fillStyle = "red";
+    ctx.fillRect(food.x, food.y, box, box);
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if( d == "LEFT") snakeX -= box;
+    if( d == "UP") snakeY -= box;
+    if( d == "RIGHT") snakeX += box;
+    if( d == "DOWN") snakeY += box;
+
+    if(snakeX == food.x && snakeY == food.y) {
+        food = { x: Math.floor(Math.random() * 19 + 1) * box, y: Math.floor(Math.random() * 19 + 1) * box };
+    } else {
+        snake.pop();
+    }
+
+    let newHead = { x: snakeX, y: snakeY };
+
+    // Game Over si collision mur ou corps
+    if(snakeX < 0 || snakeY < 0 || snakeX >= canvas.width || snakeY >= canvas.height) {
+        clearInterval(game);
+        alert("Game Over !");
+        location.reload();
+    }
+
+    snake.unshift(newHead);
+}
+
+let game = setInterval(draw, 100);
